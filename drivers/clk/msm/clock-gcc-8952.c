efc12d79234cd5e008cb26500f960c969ae00f87
DyWN <dywndev@gmail.com>
2018-02-20 15:38:47 +0100

    use 550MHz - more stable

--- drivers/clk/msm/clock-gcc-8952.c
+++ drivers/clk/msm/clock-gcc-8952.c
@@ -743,7 +743,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk_8937[] = {
 	F_SLEW( 400000000, FIXED_CLK_SRC, gpll0,	2,	0,	0),
 	F_SLEW( 450000000, 900000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 500000000, 1000000000,	  gpll3,	1,	0,	0),
-	F_SLEW( 600000000, 1200000000,	  gpll3,	1,	0,	0),
+	F_SLEW( 550000000, 1100000000,	  gpll3,	1,	0,	0),
 	F_END
 };
 
@@ -4419,7 +4419,7 @@ static int msm_gcc_probe(struct platform_device *pdev)
 				gfx3d_clk_src.freq_tbl =
 					ftbl_gcc_oxili_gfx3d_clk_8937;
 				gfx3d_clk_src.c.fmax[VDD_DIG_SUPER_TUR] =
-								600000000;
+								550000000;
 			}
 		}
 	} else if (compat_bin2 || compat_bin4) {
